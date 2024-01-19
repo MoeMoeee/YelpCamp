c7ad3a810c6da7a006ccea41157175db3d2789b9
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2024-01-19 15:55:33 +0700

    set up cloudinary + multer

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,20 +4,21 @@ const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const camp = require('../controllers/campgrounds')
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const {storage} = require('../cloudinary')
+const upload = multer({ storage })
 
 
 router.get('/', catchAsync(camp.renderCampground));
 
 router.get('/new', isLoggedIn, camp.renderNewCampground)
 
-router.post('/',isLoggedIn, validateCampground, catchAsync(camp.createCampground))
+// router.post('/',isLoggedIn, validateCampground, catchAsync(camp.createCampground))
 
 
 // test connecting multer
-// router.post('/', upload.array('image'), (req, res) => {
-//     console.log(req.body, req.files);
-// })
+router.post('/', upload.array('image'), (req, res) => {
+    console.log(req.body, req.files);
+})
 
 router.get('/:id', catchAsync(camp.getCampground));
 
