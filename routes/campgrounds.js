99a67109d201078b575573dcbd182d2ab173c905
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2024-01-19 15:29:31 +0700

    install multer to store image + set up env

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const Campground = require('../models/campground');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const camp = require('../controllers/campgrounds')
-
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 
 router.get('/', catchAsync(camp.renderCampground));
@@ -12,6 +12,12 @@ router.get('/', catchAsync(camp.renderCampground));
 router.get('/new', isLoggedIn, camp.renderNewCampground)
 
 router.post('/',isLoggedIn, validateCampground, catchAsync(camp.createCampground))
+
+
+// test connecting multer
+// router.post('/', upload.array('image'), (req, res) => {
+//     console.log(req.body, req.files);
+// })
 
 router.get('/:id', catchAsync(camp.getCampground));
 
