4e79f8fb0c4cef40e6bc1fd3a9912d882530f31a
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2024-01-24 13:54:09 +0700

    added marker on map based on camp location

--- public/javascripts/showMap.js
+++ public/javascripts/showMap.js
@@ -2,6 +2,12 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40] , // starting position [lng, lat]
-    zoom: 8, // starting zoom
+    center: campground.geometry.coordinates , // starting position [lng, lat]
+    zoom: 13, // starting zoom
 });
+
+// Set marker options.
+const marker = new mapboxgl.Marker({
+    color: "#B22222",
+    draggable: true
+}).setLngLat(campground.geometry.coordinates).addTo(map);
